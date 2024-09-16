a0e5485dcb7232493bcfa7f2a33a6940111cc9c9
batuhancinar <batuhanfaruk1998@gmail.com>
2024-09-16 06:25:14 +0300

    palindrome sayılar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,21 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
     }
+
+    public static boolean isPalindrome(int number){
+        number=Math.abs(number);
+        char[] digits = String.valueOf(number).toCharArray();
+        String reversed = "";
+        for(int i = digits.length - 1; i >=0; i--) {
+            reversed += digits[i];
+
+
+        }
+        return reversed.equalsIgnoreCase(String.valueOf(number));
+
+    }
+
 }
